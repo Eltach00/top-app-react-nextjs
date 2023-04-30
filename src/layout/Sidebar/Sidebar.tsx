5b712fdcd9a100f58f9bccdf35161c4ad884aa6d
Eltach00 <eltacquliyev@gmail.com>
2023-04-30 21:27:07 +0400

    sidebar

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
 import { Menu } from "../Menu/Menu";
 import { SidebarProps } from "./Sidebar.props";
+import Logo from "../logo.svg";
+import cn from "classnames";
+import styles from "./Sidebar.module.css";
 
-export function Sidebar({ ...props }: SidebarProps): JSX.Element {
+export function Sidebar({ className, ...props }: SidebarProps): JSX.Element {
   return (
-    <div {...props}>
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div> Search</div>
       <Menu />
     </div>
   );
